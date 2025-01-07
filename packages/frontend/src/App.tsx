bff84492b14607e560eeb35492abf3fc9105a43f
Micha de Vries <micha@devrie.sh>
2025-01-07 22:09:45 +0000

    editor component

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import "@blocknote/core/fonts/inter.css";
-import { BlockNoteView } from "@blocknote/mantine";
-import "@blocknote/mantine/style.css";
-import { useCreateBlockNote } from "@blocknote/react";
- 
+import Editor, { useEditor } from "./components/Editor";
+
 export default function App() {
-  // Creates a new editor instance.
-  const editor = useCreateBlockNote();
- 
-  // Renders the editor instance using a React component.
-  return <BlockNoteView editor={editor} />;
+	const editorState = useEditor();
+
+	return (
+		<>
+			<Editor {...editorState} />
+			<pre>{JSON.stringify(editorState.blocks, null, 2)}</pre>
+		</>
+	);
 }
