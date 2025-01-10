a171d53914eadeb213cbf726a608ccbd2d4b982b
Micha de Vries <micha@devrie.sh>
2025-01-10 13:06:16 +0000

    remove editorjs migration

--- packages/frontend/src/schema/migrations/index.ts
+++ packages/frontend/src/schema/migrations/index.ts
@@ -1,2 +1,1 @@
 export { default as mig1 } from "./mig1-initial-page-schema.ts";
-export { default as mig2 } from "./mig2-migrate-to-editorjs.ts";
--- packages/frontend/src/schema/migrations/mig2-migrate-to-editorjs.ts
+++
@@ -1,9 +0,0 @@
-import { surql } from "surrealdb";
-
-export default surql`
-    DEFINE FIELD OVERWRITE content ON page TYPE array<{
-        id: string,
-        type: string,
-        data: object,
-    }>;
-`;
