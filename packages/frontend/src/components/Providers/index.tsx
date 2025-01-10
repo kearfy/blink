59ab8e7126bc5aad0f290e418e68e22158100dfe
Micha de Vries <micha@devrie.sh>
2025-01-10 09:45:26 +0000

    Redo providers (again)

--- packages/frontend/src/components/Providers/index.tsx
+++ packages/frontend/src/components/Providers/index.tsx
@@ -1,14 +1,32 @@
 import type { ReactNode } from "react";
-import { MantineProvider } from "./mantine";
 import { SurrealProvider } from "./surreal";
-import { TanstackQueryProvider } from "./tanstack-query";
+import { surrealdbWasmEngines } from "@surrealdb/wasm";
+import Surreal from "surrealdb";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+const surrealClient = new Surreal({
+    engines: surrealdbWasmEngines()
+});
+
+import { MANTINE_THEME } from "../../utils/mantine";
+import { MantineProvider } from "@mantine/core";
 
 export function Providers({ children }: { children: ReactNode }) {
 	return (
-		<SurrealProvider>
-			<TanstackQueryProvider>
-				<MantineProvider>{children}</MantineProvider>
-			</TanstackQueryProvider>
-		</SurrealProvider>
+        <QueryClientProvider client={queryClient}>
+            <SurrealProvider 
+                client={surrealClient}
+                endpoint="indxdb://blink" 
+                params={{
+                    namespace: "blink",
+                    database: "blink",
+                }}
+            >
+                <MantineProvider theme={MANTINE_THEME}>
+                    {children}
+                </MantineProvider>
+            </SurrealProvider>
+        </QueryClientProvider>
 	);
 }
